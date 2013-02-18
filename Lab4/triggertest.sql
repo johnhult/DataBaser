64da9c6a8485f6919ee06abae5382a1cee2320bd
Anton Lindgren <lindgren.anton@gmail.com>
2013-02-18 16:50:49 +0100

    Triggertests more complete

--- Lab4/triggertest.sql
+++ Lab4/triggertest.sql
@@ -4,8 +4,15 @@ INSERT INTO RestrictedCourses VALUES ('RST000', 2);
 
 INSERT INTO Registrations VALUES ('0000000001', 'RST000', 'Registered');
 INSERT INTO Registrations VALUES ('0000000002', 'RST000', 'Registered');
-INSERT INTO Registrations VALUES ('0000000003', 'RST000', 'Registered');
 INSERT INTO Registrations VALUES ('0000000004', 'RST000', 'Registered');
+INSERT INTO Registrations VALUES ('0000000003', 'RST000', 'Registered');
+
+/* Should not work */
+INSERT INTO Registrations VALUES ('0000000001', 'RST000', 'Registered');
+
+/* Should not be registered or put in waiting list */
+INSERT INTO Read VALUES ('0000000005', 'RST000', '5');
+INSERT INTO Registrations VALUES ('0000000005', 'RST000', 'Registered');
 
 /* 1 and 2 should be registered, 3 and 4 should be waiting */
 SELECT * FROM Registrations WHERE course = 'RST000';
