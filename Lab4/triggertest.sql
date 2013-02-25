0949a8de6f1d28be36628909bf1f770057cf7188
John <johnhult89@gmail.com>
2013-02-25 10:35:41 +0100

    Some changes for the triggertest

--- Lab4/triggertest.sql
+++ Lab4/triggertest.sql
@@ -6,10 +6,12 @@ INSERT INTO RestrictedCourses VALUES ('RST000', 2);
 INSERT INTO Courses VALUES ('URS000', 10, 'Kemi');
 
 /* Registrations for restricted course, 1 and 2 should be registered
- * 3 should be waiting with queue position 1, 4 waiting with pos 2
- */
+	3 should be waiting with queue position 1, 4 waiting with pos 2 */
 INSERT INTO Registrations VALUES ('0000000001', 'RST000', 'Registered');
 INSERT INTO Registrations VALUES ('0000000002', 'RST000', 'Registered');
+
+/* OBS! YOU CAN NOT INSERT THESE TWO AT THE SAME TIME DUE TO
+	DATE UNIQUENESS! */
 INSERT INTO Registrations VALUES ('0000000003', 'RST000', 'Registered');
 INSERT INTO Registrations VALUES ('0000000004', 'RST000', 'Registered');
 
@@ -30,3 +32,18 @@ INSERT INTO Registrations VALUES ('0000000005', 'RST000', 'Registered');
 /* Views displaying data above, read comments for how it should look */
 SELECT * FROM Registrations WHERE course = 'RST000' OR course = 'URS000' ORDER BY course;
 SELECT * FROM CourseQueuePositions WHERE course = 'RST000' OR course = 'URS000' ORDER BY course; 
+
+/* Start by deleting from a course that is not restricted */
+DELETE FROM Registrations WHERE studentId = '0000000004' AND course = 'URS000';
+
+/* Delete someone that is 'Registered' in a restricted queue which should
+	lead to studentId '0000000003' being 'Registered' for the course */
+DELETE FROM Registrations WHERE studentId = '0000000002' AND course = 'RST000';
+
+/* Delete from someone that is 'Waiting' and it should do nothing more 
+	than delete that row */
+DELETE FROM Registrations WHERE studentId = '0000000004' AND course = 'RST000';
+
+/* Delete person that is 'Registered' when we don't have anyone in queue
+	to make sure we handle empty queues */
+DELETE FROM Registrations WHERE studentId = '0000000003' AND course = 'RST000';
